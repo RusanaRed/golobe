1ac41e31983520c0befec1942b7f17788956c02e
Anastasia <semenova.nastenka@gmail.com>
2025-10-24 21:14:14 +0300

    Reorganized scss files: filters/filter

--- src/js/modules/filters-toggle.js
+++ src/js/modules/filters-toggle.js
@@ -1,16 +1,16 @@
-const filtersSectionToggles = document.querySelectorAll('.filters__collapse-button');
-const filterSections = document.querySelectorAll('.filters__group');
+const filtersSectionToggles = document.querySelectorAll('.filter__collapse-button');
+const filterSections = document.querySelectorAll('.filters__filter');
 
 filterSections.forEach(onToggleClick);
 
 function onToggleClick(item) {
   const currentFilterSection = item;
-  const filterSectionToggle = currentFilterSection.querySelector('.filters__collapse-button');
+  const filterSectionToggle = currentFilterSection.querySelector('.filter__collapse-button');
 
   filterSectionToggle.addEventListener('click', function(event) {
     event.preventDefault();
 
-    const filtersContent = currentFilterSection.querySelector('.filters__filter-content');
+    const filtersContent = currentFilterSection.querySelector('.filter__content');
     
     filtersContent.classList.toggle('close');
     filterSectionToggle.classList.toggle('collapse');
